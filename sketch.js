a2236c2de1a6a4971431351893b357adda8fcff8
Te'jon Roy <troy@wlapcs.org>
2017-03-03 17:21:36 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,33 @@
+function setup(){
+    createCanvas(600,400);
+    background(100,100,100);
+}
+
+function draw(){
+     line(35,80,400,200);
+     
+     stroke(255,255,255);
+    fill(255,0,0);
+    rect(290,170,200,250);
+    
+    fill(255,0,0);
+    rect(100,200,300,350);
+
+stroke(0,0,0);
+strokeWeight(4);
+    fill(0,0,255);
+    ellipse(50,250,80,80);
+    
+    fill(0,0,255);
+    ellipse(20,220,100,100);
+    
+   line(20,50,250,300);
+   line(5,40,-230,-500);
+   line(-30,-20,100,300);
+   line(50,150,200,300);
+   
+   
+   
+   
+}
+
